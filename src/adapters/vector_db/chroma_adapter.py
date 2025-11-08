ca349bee78a681012bef77632ce8b024a676c6dc
Huy Nguyen <huynnthe176346@fpt.edu.vn>
2025-11-08 11:49:48 +0700

    commit

--- src/adapters/vector_db/chroma_adapter.py
+++ src/adapters/vector_db/chroma_adapter.py
@@ -1,21 +1,51 @@
 
+import os
+import chromadb
+import json
+from datetime import datetime
+from langchain_openai import OpenAIEmbeddings
 from abc import ABC, abstractmethod
 from typing import List, Dict, Any, Optional
 from uuid import UUID
 from ...domain.ports.vector_search_port import VectorSearchPort
 
-def create_metadata_from_summary(summary: str) -> str:
-    """
-    Tạo metadata từ summary (nội dung tóm tắt CV)
+CHROMA_PATH = os.path.join(os.path.dirname(__file__), "..", "chroma_db")
+COLLECTION_NAME = "cv_embedding"
 
-    Args:
-        summary (str): Nội dung tóm tắt CV
+embeddings_client = OpenAIEmbeddings(model="text-embedding-3-small", api_key=os.getenv("TEXT_EMBEDDING_API_KEY"))
 
-    Returns:
-        str: Metadata dưới dạng chuỗi JSON
-    """
-    # Giả sử summary đã là một chuỗi JSON hợp lệ
+chromaDB_client = chromadb.PersistentClient(path=CHROMA_PATH)
+cv_collection = chromaDB_client.get_or_create_collection(name=COLLECTION_NAME)
+
+def create_metadata_from_summary(summary: str) -> Dict[str, Any]:
+    summarized_info = summary
+    skills = json.loads(summary).get("skills", [])
+    experience_years = json.loads(summary).get("experience", 0)
+    education_level = json.loads(summary).get("education_level", "N/A")
+    savetime = datetime.now().isoformat()
+
+    metadata = {
+        "summary": summarized_info,
+        "skills": skills,
+        "experience_years": experience_years,
+        "education_level": education_level,
+        "saved_at": savetime
+    }
     return metadata
+
+def create_embedding_info_from_summary(summary: str) -> List[float]:
+    text = f"""
+    Candidate Summary: {summary}
+    Skills: {', '.join(json.loads(summary).get("skills", []))}
+    Experience: {json.loads(summary).get("experience", 0)} years
+    Education Level: {json.loads(summary).get("education_level", "N/A")}
+    """.strip()
+    try:
+        embedding = embeddings_client.embed_query(text)
+    except Exception as e:
+        print(f"Error generating embedding: {e}")
+        embedding = []
+    return embedding
 
 class ChromaAdapter(VectorSearchPort):
     """Interface for vector database operations.
@@ -34,4 +64,15 @@ class ChromaAdapter(VectorSearchPort):
         self.embedding_model = embedding_model
         self.index_name = index_name
 
-    
+    metadatas = create_metadata_from_summary(summary="")
+    embedding = create_embedding_info_from_summary(summary="")
+
+    async def store_cv_embedding(self, cv_analysis_id, embedding, metadatas):
+        try:
+            cv_collection.add(
+                ids=[cv_analysis_id],
+                embeddings=[embedding],
+                metadatas=[metadatas]
+            )
+        except Exception as e:
+            print(f"Error storing CV embedding: {e}")
